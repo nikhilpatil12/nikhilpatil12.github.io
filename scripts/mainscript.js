0993f4c28493f9f67f028652f2feb7f30cc6605b
nikhilpatil12 <patil.nikhil12@outlook.com>
2022-07-15 23:46:38 +0530

    Alpha 1

--- scripts/mainscript.js
+++ scripts/mainscript.js
@@ -1,5 +1,7 @@
 function openNav() {
     document.getElementById("divPopup").style.height = "100%";
+    $("#divHamBurgerButton").hide();
+    
     // $("#new")[0].style.height = "100%";
 
     // document.getElementById("new").style.height = "100%";
@@ -7,18 +9,27 @@ function openNav() {
 }
 function closeNav() {
     document.getElementById("divPopup").style.height = "0%";
+    $("#divHamBurgerButton").show();
+
     // $("#new")[0].style.height = "0%";
 
     // document.getElementById("new").style.height = "0%";
     // document.getElementById("divSide").style.visibility = "collapse";
 
 }
+
 window.addEventListener('resize', function(event) {
     if(event.target.outerWidth<576)
-    {
         closeNav();
-    }
     else
         openNav();
 }, true);
-  
+// $(document).ready(function() { 
+//     closeNav();
+    
+// });
+window.addEventListener('load', 
+  function() { 
+    if(window.outerWidth<576)
+        closeNav();
+  }, false);
